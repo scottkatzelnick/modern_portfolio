d64ecfd0c5dfde603c17dcae0ee88da25369482d
Scott Katzelnick <scott@devsmk.com>
2020-11-25 01:27:15 -0500

    Animated burger menu

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+// Select DOM Items
+const menuButton = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Burger Menu Click Event
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('close');
+  menu.classList.toggle('show');
+  menuNav.classList.toggle('show');
+  menuBranding.classList.toggle('show');
+  navItems.forEach((item) => {
+    item.classList.toggle('show');
+  });
+});
