6bfa2c93415d655c8e38f071391c697ff809ead5
Scott Katzelnick <scott@devsmk.com>
2020-11-27 16:40:30 -0500

    Prevented redirect of form submisson and display user message

--- dist/js/inputmask.js
+++ dist/js/inputmask.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
   $('#phone').mask('000-000-0000');
 });
-
-// reCaptcha Form Requirement
-window.removeFakeCaptcha = function () {
-  document.querySelector('.captcha-fake-field').remove();
-};
--- dist/js/main.js
+++ dist/js/main.js
@@ -18,3 +18,50 @@ menuButton.addEventListener('click', () => {
 
 // Read User Browser For CSS Compatability Issues
 document.documentElement.setAttribute('data-browser', navigator.userAgent);
+
+// Formspree.io AJAX form submission with user status message
+window.addEventListener('DOMContentLoaded', function () {
+  var form = document.querySelector('#contact-form');
+  var message = document.querySelector('.submit-msg');
+
+  function success() {
+    form.reset();
+    message.classList.add('display-msg');
+    message.innerHTML = 'Thanks! Message Sent!';
+    setTimeout(function () {
+      message.innerHTML = '';
+      message.classList.remove('display-msg');
+    }, 8000);
+  }
+
+  function error() {
+    message.classList.add('display-error');
+    message.innerHTML =
+      'Oops, your form submission hit a snag! Please try again.';
+    setTimeout(function () {
+      message.innerHTML = '';
+      message.classList.remove('display-error');
+    }, 8000);
+  }
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
